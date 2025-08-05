9e6afdaa34f05c3c3d93ab4c03bc2041d4c3bd91
Nobody <157801921+GodFossil@users.noreply.github.com>
2025-08-04 19:59:57 -0500

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,31 @@
-import chalk from 'chalk';
-export default {
- info : (...m) => console.log(chalk.blue('[INFO]'), ...m),
- debug: (...m) => console.log(chalk.gray('[DEBUG]'), ...m),
- warn : (...m) => console.warn(chalk.yellow('[WARN]'), ...m),
- error: (...m) => console.error(chalk.red('[ERR]'), ...m)
-};
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'discord-factcheck-bot' },
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ]
+});
+
+// Create logs directory if it doesn't exist
+const fs = require('fs');
+const path = require('path');
+const logsDir = path.join(__dirname, 'logs');
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir, { recursive: true });
+}
+
+module.exports = logger;
